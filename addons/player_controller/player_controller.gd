93afa2302769b8cde6949f20db0eef4f61df541b
Devin <devin@nowyoujump.com>
2025-09-13 19:07:51 -0700

    placeholder ui

--- addons/player_controller/player_controller.gd
+++ addons/player_controller/player_controller.gd
@@ -2,6 +2,11 @@ class_name PlayerController extends Controller
 
 @export var player: Player
 
+func get_local_player() -> LocalPlayer:
+	if player is LocalPlayer:
+		return player
+	return null
+
 func get_hud() -> Control:
 	if player is LocalPlayer:
 		return player.hud
--- levels/main.gd
+++ levels/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var tilemap: TileMapLayer
+@export var player_ui_scene: PackedScene
 
 func _enter_tree() -> void:
 	Players.player_joined.connect(_player_joined)
@@ -8,7 +9,14 @@ func _enter_tree() -> void:
 func _exit_tree() -> void:
 	Players.player_joined.disconnect(_player_joined)
 
-func _player_joined(_controller: PlayerController) -> void:
+func _player_joined(controller: PlayerController) -> void:
+	var player := controller.get_local_player()
+	if player_ui_scene and player:
+		player.hud = player_ui_scene.instantiate()
+		var canvas_layer := CanvasLayer.new()
+		canvas_layer.add_child(player.hud)
+		canvas_layer.layer = 10
+		player.get_viewport().add_child(canvas_layer)
 	_init_game()
 
 func _init_game() -> void:
