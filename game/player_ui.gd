e3c4718a9e0254e5124c43080118587981db0658
Devin <devin@nowyoujump.com>
2025-09-15 02:27:46 -0700

    ui updates

--- game/player_ui.gd
+++ game/player_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var points_text: RichTextLabel
+@export var autoclickers_container: Control
 @export var autoclickers_text: RichTextLabel
 
 func _notification(what: int) -> void:
@@ -17,5 +18,5 @@ func _update_points(points: float) -> void:
 	points_text.text = "$ %s" % NumFormat.format_points(points)
 
 func _update_autoclickers(autoclickers: int) -> void:
-	autoclickers_text.text = "Autoclickers: %s" % autoclickers
-	autoclickers_text.visible = autoclickers > 0
+	autoclickers_text.text = "Auto clickers: %s" % autoclickers
+	autoclickers_container.visible = autoclickers > 0
