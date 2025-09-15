6d22015e8a4f78b2c6ce4a3c10422871777daa55
Devin <devin@nowyoujump.com>
2025-09-14 17:04:58 -0700

    add damage_all command

--- game/game_commands.gd
+++ game/game_commands.gd
@@ -3,3 +3,6 @@ class_name GameCommands extends ConsoleCommands
 func set_points(amount: float) -> void:
 	var controller: ExpansionPlayerController = Players.get_primary_controller()
 	controller.player_state.points = amount
+
+func damage_all(damage: float = 1.0) -> void:
+	get_tree().root.propagate_call("apply_damage", [damage])
