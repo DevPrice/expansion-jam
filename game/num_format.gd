788d378ea207852ed349e9220eeba112c587b842
Devin <devin@nowyoujump.com>
2025-09-16 02:31:34 -0700

    fix number formatting

--- game/num_format.gd
+++ game/num_format.gd
@@ -3,5 +3,4 @@ class_name NumFormat extends Node
 static func format_points(points: float) -> String:
 	if points > 1_000_000_000_000_000.0:
 		return Strings.scientific(points)
-	# TODO: Avoid ints
-	return Strings.format_int(floori(points))
+	return Strings.format_separators(points, "%0.0f")
