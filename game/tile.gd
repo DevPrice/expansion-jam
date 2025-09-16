a3129595e60beac0d4e2da787a887f3415d7fdb5
Devin <devin@nowyoujump.com>
2025-09-15 19:37:44 -0700

    damage animation

--- game/tile.gd
+++ game/tile.gd
@@ -9,10 +9,16 @@ signal destroyed
 
 @export var point_value: float = 1
 
+@export var _animation_player: AnimationPlayer
+
 func apply_damage(damage: float) -> void:
 	var start_hp := hp
 	hp -= max(0, damage)
 	var dealt := start_hp - hp
 	damaged.emit(dealt)
-	if start_hp > 0 and is_zero_approx(hp):
-		destroyed.emit()
+	if dealt > 0:
+		_animation_player.stop()
+		_animation_player.play("damage")
+		if is_zero_approx(hp):
+			$TextureRect.position = Vector2(50, 50)
+			destroyed.emit()
