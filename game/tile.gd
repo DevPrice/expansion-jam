26dec9e87fc020827d35f367b9d0d6a0862b1ad3
Devin <devin@nowyoujump.com>
2025-09-17 16:56:32 -0700

    simple damage visual

--- game/tile.gd
+++ game/tile.gd
@@ -6,10 +6,17 @@ signal destroyed
 @export var hp: float = 1:
 	set(value):
 		hp = maxf(0, value)
+		_max_hp = maxf(_max_hp, value)
+		propagate_call("set_instance_shader_parameter", ["damage", 1.0 - hp / _max_hp])
 
 @export var point_value: float = 1
 
 @export var _animation_player: AnimationPlayer
+
+var _max_hp: float = hp
+
+func _ready() -> void:
+	propagate_call("set_instance_shader_parameter", ["instance_id", randi() % 10000])
 
 func apply_damage(damage: float) -> void:
 	var start_hp := hp
@@ -20,5 +27,4 @@ func apply_damage(damage: float) -> void:
 		_animation_player.stop()
 		_animation_player.play("damage")
 		if is_zero_approx(hp):
-			$TextureRect.position = Vector2(50, 50)
 			destroyed.emit()
